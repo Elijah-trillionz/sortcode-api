29df7044277ad6daab97c87f8dbae54adb21d4d9
Elijah Trillionz <60356516+Elijah-trillionz@users.noreply.github.com>
2023-03-11 08:22:22 +0100

    removed unsupported mongoose option

--- config/mongodb.js
+++ config/mongodb.js
@@ -1,16 +1,15 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 async function connectDB() {
   try {
     const dbConnection = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
-      useFindAndModify: false,
       useUnifiedTopology: true,
     });
 
-    console.log('database connected to ' + dbConnection.connection.host);
+    console.log("database connected to " + dbConnection.connection.host);
   } catch (err) {
-    console.log(err)
+    console.log(err);
     process.exit(1);
   }
 }
